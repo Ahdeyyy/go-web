8abd15540d02626c60f537f8d9f40c4408dfb2b9
Ahdeyyy <85193076+Ahdeyyy@users.noreply.github.com>
2022-11-18 09:41:29 +0100

    graceful server shutdown

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
 	"net/http"
 	"os"
 	"os/signal"
+	"time"
 
 	"github.com/Ahdeyyy/go-web/internal/config"
 	"github.com/Ahdeyyy/go-web/internal/handlers"
@@ -20,10 +23,17 @@ const portNumber = ":8080"
 // main is the entry point for the application
 func main() {
 
-	// create a new server
+	var wait time.Duration
+	flag.DurationVar(&wait, "graceful-timeout", time.Second*15, "the duration for which the server gracefully wait for existing connections to finish - e.g. 15s or 1m")
+	flag.Parse()
+
+	// create a new server mux and register the routes
 	srv := &http.Server{
-		Addr:    appConfig.Port,
-		Handler: routes.Routes(&appConfig),
+		Addr:         appConfig.Port,
+		WriteTimeout: time.Second * 15, // 15 seconds write timeout
+		ReadTimeout:  time.Second * 15, // 15 seconds read timeout
+		IdleTimeout:  time.Second * 60, // 60 seconds idle timeout
+		Handler:      routes.Routes(&appConfig),
 	}
 
 	// start the server
@@ -31,19 +41,34 @@ func main() {
 		log.Println("Starting server on port", appConfig.Port)
 		err := srv.ListenAndServe()
 		if err != nil {
-			panic(err)
+			log.Println(err)
 		}
 	}()
 
-	// wait for a signal to shutdown
-	quit := make(chan os.Signal, 1)
-	signal.Notify(quit, os.Interrupt)
-	<-quit
-	log.Println("Shutting down server...")
+	c := make(chan os.Signal, 1)
+	// We'll accept graceful shutdowns when quit via SIGINT (Ctrl+C)
+	// SIGKILL, SIGQUIT or SIGTERM (Ctrl+/) will not be caught.
+	signal.Notify(c, os.Interrupt)
+
+	// Block until we receive our signal.
+	<-c
+
+	// Create a deadline to wait for.
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+	// Doesn't block if no connections, but will otherwise wait
+	// until the timeout deadline.
+	srv.Shutdown(ctx)
+	// Optionally, you could run srv.Shutdown in a goroutine and block on
+	// <-ctx.Done() if your application should wait for other services
+	// to finalize based on context cancellation.
+	log.Println("shutting down")
+	os.Exit(0)
 
 }
 
 func init() {
+
 	// change this to false when in production
 	appConfig.Debug = true
 
